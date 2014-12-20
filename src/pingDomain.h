6104332b493db63a03c69355b33b2da9360d8451
sebysira <seby.siragusa@gmail.com>
2014-12-20 16:23:36 +0100

    add db.h

---
+++ src/pingDomain.h
@@ -0,0 +1,28 @@
+/*
+This file is part of MailParser package.
+Writen by
+        - Danilo Cantarella (https://github.com/Flyer-90);
+        - Sebastiano Siragusa (https://github.com/sebysira);
+        - Filippo Randazzo (https://github.com/filirnd);
+
+Full MailParser is released by GPL3 licence.
+*/
+
+
+#include "config.h"
+
+int ping(char* domain) {
+    char statement[MAX];
+    sprintf(statement, "ping -c 1 -w 5 %s", domain);
+    return ping;
+}
+
+/*
+This file is part of MailParser package.
+Writen by
+        - Danilo Cantarella (https://github.com/Flyer-90);
+        - Sebastiano Siragusa (https://github.com/sebysira);
+        - Filippo Randazzo (https://github.com/filirnd);
+
+Full MailParser is released by GPL3 licence.
+*/
