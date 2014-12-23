95b50d0f660f58099c4bb86fb6af0841d901c3c0
Sebastiano Siragusa <seby.siragusa@gmail.com>
2014-12-23 16:05:50 +0100

    Update config.h

--- src/config.h
+++ src/config.h
@@ -32,11 +32,11 @@ Full MailParser is released by GPL3 licence.
 #define reg_domain "(com|net|org|edu|biz|gov|mil|info|name|me|tv|us|mob|academy|ad|ae|ag|ai|al|am|ar|at|au|aw|ba|be|bg|bh|bm|bn|bo|br|bs|bw|by|bz|ca|ch|ci|cl|cn|co|company|cr|cu|cy|cz|de|dk|dm|do|ec|ee|eg|es|et|fi|fj|fm|fo|fr|gb|ge|gi|gl|gr|gt|gu|gy|hk|hn|hr|hu|id|ie|il|in|ir|is|it|jm|jo|jp|ke|kg|kr|kw|kz|lb|lc|li|lk|lt|lu|lv|ma|mc|md|mk|mn|mo|mt|mu|museum|mv|mx|my|na|nc|ng|ni|nl|no|np|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pt|py|qa|ro|ru|sa|se|sg|si|sk|su|sv|th|tn|tr|tt|tw|tz|ua|ug|uk|us|uy|uz|va|ve|vi|vn|ws|ye|yu|za|zm|zw)"
 
 
-/* config mysql database */
+/* config you mysql database */
 
 #define server "localhost"
-#define user "root"
-#define password "12121989"
+#define user "your_username"
+#define password "your_password"
 
 
 /*
